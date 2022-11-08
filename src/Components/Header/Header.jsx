c2b7a3f28a096f7d6fe1223a540f682887a38832
Md Rifat Hossen Saown <rifatsaown0@gmail.com>
2022-11-08 19:34:51 +0600

    logo added

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './Header.css';
+import logo from "../../images/Logo.svg";
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div>
+      <div>
+        <img src={logo} alt="" />
+      </div>
+    </div>
   )
 }
