10dfe04fb783cb67ffaccbc4ce788d05b18e623e
Md Rifat Hossen Saown <rifatsaown0@gmail.com>
2022-11-09 22:59:17 +0600

    Data show motamoti done

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import './Product.css';
 
-export default function Product() {
+export default function Product(props) {
+  const {name , seller , price , ratings , img}=props.product;;
   return (
-    <div>Product</div>
+    <div className='product'>
+      <img src={img} alt="" />
+      <h3>{name}</h3>
+      <p>By: {seller}</p>
+      <p>${price}</p>
+      <p>Rating: {ratings}</p>
+      <button className='btn-cart'>add to cart</button>
+    </div>
   )
 }
