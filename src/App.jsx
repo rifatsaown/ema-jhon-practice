6d032da1693f051bd035c78ba30620a493fb926b
Md Rifat Hossen Saown <rifatsaown0@gmail.com>
2022-11-09 22:25:58 +0600

    shop component loaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Components/Header/Header";
+import Shop from "./Components/Shop/Shop";
 
 export default function App() {
   return (
     <div>
       <Header />
+      <Shop />
     </div>
   );
 }
--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
-import './Product.css';
+import './Shop.css';
 
 export default function Shop() {
   return (
-    <div>Shop</div>
+    <div className='shop'>
+      <div className='product-component'>
+        This is Product
+      </div>
+      <div className='shop-component'>
+        This is for cart items
+      </div>
+    </div>
   )
 }
