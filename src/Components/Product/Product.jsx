15177e2a2fa9de29e2f3043c8c73360b74ebde9f
Md Rifat Hossen Saown <rifatsaown0@gmail.com>
2022-11-09 23:16:26 +0600

    Data show done

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
-import './Product.css';
+import React from "react";
+import "./Product.css";
 
 export default function Product(props) {
-  const {name , seller , price , ratings , img}=props.product;;
+  const { name, seller, price, ratings, img } = props.product;
   return (
-    <div className='product'>
+    <div className="product">
       <img src={img} alt="" />
-      <h3>{name}</h3>
-      <p>By: {seller}</p>
-      <p>${price}</p>
-      <p>Rating: {ratings}</p>
-      <button className='btn-cart'>add to cart</button>
+      <div className="product-info">
+        <h3>{name}</h3>
+        <p className="product-price">Price : ${price}</p>
+        <small>
+          <p>Manufacter : {seller}</p>
+          <p>Rating: {ratings} Star</p>
+        </small>
+      </div>
+      <button className="btn-cart">Add to Cart</button>
     </div>
-  )
+  );
 }
