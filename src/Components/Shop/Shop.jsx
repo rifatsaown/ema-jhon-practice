2b2ba7bb8b976e071563662150ea53fe42194917
Md Rifat Hossen Saown <rifatsaown0@gmail.com>
2022-11-09 22:38:43 +0600

    fakedata loaded

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
-import './Shop.css';
+import React from "react";
+import Product from "../Product/Product";
+import "./Shop.css";
 
 export default function Shop() {
+  const [products, setProducts] = React.useState([]);
+  React.useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((json) => setProducts(json));
+  }, []);
+
   return (
-    <div className='shop'>
-      <div className='product-component'>
-        This is Product
+    <div className="shop">
+      <div className="product-component">
+        {products.map((product) => (
+          <Product product={product} key={product.id}/>
+        ))}
       </div>
-      <div className='shop-component'>
-        This is for cart items
+      <div className="shop-component">
+        <h3>Order Summary</h3>
       </div>
     </div>
-  )
+  );
 }
