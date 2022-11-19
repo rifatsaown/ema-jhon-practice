b385944c02617fb426cf576ec4bdc28147e28d61
Md Rifat Hossen Saown <rifatsaown0@gmail.com>
2022-11-19 22:00:18 +0600

    header file

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
 import logo from "../../images/Logo.svg";
 
 export default function Header() {
   return (
-    <nav className='header-container'>
-      <div className='logo'>
+    <nav className="header-container">
+      <div className="logo">
         <img src={logo} alt="" />
       </div>
-      <div className='nav-links'>
-        <a href='/home'>Home</a>
-        <a href='/order'>Order</a>
-        <a href='/inventory'>Inventory</a>
-        <a href='/login'>Login</a>
+      <div className="nav-links">
+        <a href="/home">Home</a>
+        <a href="/order">Order</a>
+        <a href="/inventory">Inventory</a>
+        <a href="/login">Login</a>
       </div>
     </nav>
-  )
+  );
 }
